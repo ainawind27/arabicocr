a7659b524fdb2a9ff1df83931b464a82c2fb6be9
ainawind27 <ainawind27@gmail.com>
2017-10-16 15:40:49 +0700

    berhasil lagi 100 %
    single prediction udah pakai ok wrong

--- src/main/java/thesis/SingleANNPrediction.java
+++ src/main/java/thesis/SingleANNPrediction.java
@@ -2,11 +2,13 @@ package thesis;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class SingleANNPrediction {
 
 	public static void main (String[]args) throws IOException{
-		File folder = new File ("D:\\filetesting\\");
+//		File folder = new File ("D:\\filetesting\\");	
+		File folder = new File ("D:\\font tesis aina\\font times new roman");
 		File folder1 = new File(folder, "zhangsuen");
 		File featuresFile = new File(folder1, "zhangsuen.Features.json");
 		
@@ -19,6 +21,34 @@ public class SingleANNPrediction {
 		ArabicPrediction arabicprediction = new ArabicPrediction();
 		arabicprediction.arabicPrediction(featuresFile, true);
 		
+		String[] expectedResult = {"ain", "alif", "ba", "dal", "dhad",
+									"dzal", "dzo", "fa", "ghoin", "habesar",
+									"hamzah", "ha", "jim", "kaf", "kha",
+									"lamalif", "lam", "mim", "nun", "qaf",
+									"ra", "sad", "sheen", "sin", "tamarbuto",
+									"ta", "tho", "tsa", "waw", "ya", "za"};
+		
+		ArabicPrediction arabicprediction1 = new ArabicPrediction();
+		String[] prediction = arabicprediction1
+				.arabicPrediction(new File(folder, "zhangsuen\\zhangsuen.Features.json"), false);
+		System.out.println("Expected result: " + Arrays.asList(expectedResult));
+		System.out.println("Prediction: " + Arrays.asList(prediction));
+		System.out.println("Matches: " + expectedResult.equals(prediction));
+		
+		int correctCount = 0;
+		for (int i = 0; i < expectedResult.length; i++) {
+			String expected = expectedResult[i];
+			String predicted = i < prediction.length ? prediction[i] : "";
+			if (expected.equals(predicted)) {
+				System.out.println(i + " - " + expected + " -> " + predicted + " (OK)");
+				correctCount++;
+			} else {
+				System.out.println(i + " - " + expected + " -> " + predicted + " (WRONG)");
+			}
+		}
+		double accuracy = correctCount * 1.0 / expectedResult.length;
+		System.out.println("Accuracy: " + accuracy);
+
 		
 	}
 }
